80742137593ab7e95bcc8f0636b77a3115db23da
Dody Nurrahman <dodynrhmn@gmail.com>
2023-05-28 20:59:24 +0800

    Membuat Atribut dan Behavior

--- src/praktek2/Rectangle.java
+++ src/praktek2/Rectangle.java
@@ -9,5 +9,13 @@ package praktek2;
  * @author ASUS
  */
 public class Rectangle {
+    double panjang;
+    double lebar;
     
+    void cetakInfo(){
+        System.out.println("================");
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar : "+lebar);
+        System.out.println("================");
+    }
 }
